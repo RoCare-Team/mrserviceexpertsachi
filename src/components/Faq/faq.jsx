107ab49b8d2ced07adda70e2297683d469f48862
Sachi8565 <101034229+Sachi8565@users.noreply.github.com>
2025-04-22 15:41:21 +0530

    dgdgh

--- src/components/Faq/faq.jsx
+++ src/components/Faq/faq.jsx
@@ -3,37 +3,6 @@ import FaqImg from '../../assets/images/newFaqCon.webp';
 
 export default function FaqAccordion({faqItems}) {
   const [openItem, setOpenItem] = useState(0);
-  // console.log(faqItems);
-  
-    // const faqItems = [
-   
-  //   {
-  //     question: "How often should I get my RO water purifier serviced in Delhi?",
-  //     answer:
-  //       "As per the RO experts, the RO water purifier is serviced at least once or twice every year to lessen premature breakdowns in Delhi. Get in touch with one of the top-rated RO water purifier services in Delhi and get the lowest cost service. You can book fill the form on RO Care India website to book RO Service in Delhi.",
-  //   },
-  //   {
-  //     question: "What does the regular water purifier service cost in Delhi?",
-  //     answer:
-  //       "An RO water purifiers maintenance cost depends on the water purifier model and the cost of replacing the RO membrane, pre-filters and post-filters, and the labor cost. The regular water purifier service cost can be lowered with periodic maintenance.",
-  //   },
-  //   {
-  //     question: "When should you schedule a water purifier service in Delhi?",
-  //     answer:
-  //       "Any models of water purifiers need to be serviced every 6 to 12 months. Ignoring beyond 12 months can cause filter damage and stops functioning properly. The water purifier manufacturing companies mentioned the standard service period. But its great to be careful about its preventive management and services at an appropriate time by the expert professional.",
-  //   },
-  //   {
-  //     question: "Which water purifier has the lowest service cost in Delhi?",
-  //     answer:
-  //       "The adequately maintained water purifier has the minimum service cost because its spares are regularly serviced in Delhi. You can also find some low-maintenance water purifiers that are considered low-maintenance cost systems. One can also try RO AMC to lower the periodic RO service cost and extend its longevity in Delhi.",
-  //   },
-  //   {
-  //     question: "How do I know my water purifier is out of service in Delhi?",
-  //     answer:
-  //       "Once your water purifier worked few years, it lowering its capacity to purify water and needs more frequent service in Delhi. There is some presentation that gives you an indication about its service requirements. Frequent breakdowns, water leakage, water impurity, and some other indications indicate its urgent maintenance in Delhi.",
-  //   },
-  // ];
-
   
   
   return (
